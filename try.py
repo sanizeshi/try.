f2002a31e1cf9a80aaf2901afb1dda40ddf0cfff
sanizeshi <82694248+sanizeshi@users.noreply.github.com>
2021-04-29 08:51:21 +0500

    try.py
    python2 try.py

---
+++ try.py
@@ -0,0 +1,7 @@
+        <div id="ascii-gallery" class="ascii-default-style"></div>
+        <script type="text/javascript">
+             var ascii_text="RANA MZ";
+             var ascii_style="banner";
+        </script>
+        <script type="text/javascript" src="https://textart.io/gadgets/ascii.figlet.js"></script>
+                 
